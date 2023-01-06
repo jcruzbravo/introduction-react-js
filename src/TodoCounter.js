661f8aa6d445861e4d24316a101ce362159567d7
jcruzbravo2000 <jcruzbravo2000@gmail.com>
2023-01-06 09:53:12 -0500

    Change colors and texts

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -2,7 +2,7 @@ import React from "react";
 import './TodoCounter.css'
 
 function TodoCounter() {
-  return <h2 className="TodoCounter">Has completado 2 de 3 TODOs</h2>;
+  return <h2 className="TodoCounter">Your completed tasks: 2</h2>;
 }
 
 export { TodoCounter };
