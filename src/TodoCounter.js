f7e2658436fcb68fb8855d0561f27963180c2925
jcruzbravo2000 <jcruzbravo2000@gmail.com>
2023-01-06 18:55:04 -0500

    Add state handling to find and count the TODOs

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,8 +1,9 @@
 import React from "react";
 import './TodoCounter.css'
 
-function TodoCounter() {
-  return <h2 className="TodoCounter">Your completed tasks: 2</h2>;
+function TodoCounter({total, completed}) {
+
+  return <h2 className="TodoCounter">Your completed tasks: {completed} of {total}</h2>;
 }
 
 export { TodoCounter };
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,10 +1,19 @@
+  /* Estructura del useState:
+    [
+      'valor',
+      function setState (va a permitir editar nuestro estado)
+    ]
+    Hace cambios al estado del componente y le dice a React que lo vuelva a renderizar.
+    const [searchValue, setSearchValue] = React.useState('');
+  */
+
 import React from "react";
 import './TodoSearch.css';
 
-function TodoSearch() {
+function TodoSearch({ searchValue, setSearchValue }) {
 
   const onSearchValueChange = (event) => {
-    console.log(event.target.value)
+    setSearchValue(event.target.value);
   }
 
   return (
@@ -12,6 +21,7 @@ function TodoSearch() {
       <input 
         className="TodoSearch" 
         placeholder="e.g Make the coffe" 
+        value={searchValue}
         onChange={onSearchValueChange}
         />
     </div>
