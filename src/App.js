a8647034aa421bca767b4fd1b005be6151a0167c
jcruzbravo2000 <jcruzbravo2000@gmail.com>
2023-01-06 18:55:40 -0500

    Connect the components and send the information

--- src/App.js
+++ src/App.js
@@ -6,20 +6,45 @@ import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
 //import './App.css';
 
-const todos = [
+const defaultTodos = [
   { text: "Make coffe", completed: false },
   { text: "Continue taking the react course", completed: false },
   { text: "Exercise", completed: false },
 ];
 
 function App() {
+
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if(!searchValue.length >= 1 ){
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    })
+  }
+
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter 
+        total={totalTodos}
+        completed={completedTodos}
+      />
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
       
       <TodoList>
-        {todos.map((todo) => (
+        {searchedTodos.map((todo) => (
           <TodoItem 
             key={todo.text} 
             text={todo.text}
