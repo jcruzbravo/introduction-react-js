456a6138e4e25e1a2da28adf77239271e7d69bb9
jcruzbravo2000 <jcruzbravo2000@gmail.com>
2023-01-09 11:29:24 -0500

    Add components to complete and delete

---
+++ src/TodoIcon/CompleteIcon.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { TodoIcon } from './';
+
+function CompleteIcon({ completed, onComplete }){
+  return (
+    <TodoIcon
+      type="check"
+      color={completed ? '#4caf50' : 'gray'}
+      onClick={onComplete}
+    />
+  );
+}
+
+export { CompleteIcon };
+
