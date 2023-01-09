fcf282be302244fb168d771755bc038083a73e5d
jcruzbravo2000 <jcruzbravo2000@gmail.com>
2023-01-09 13:48:31 -0500

    Increase the wait by one more second

--- src/TodoContext/useLocalStorage.js
+++ src/TodoContext/useLocalStorage.js
@@ -22,7 +22,7 @@ function useLocalStorage(itemName, initialValue) {
 
         setItem(parsedItem);
         setLoading(false);
-      }, 4000);
+      }, 5000);
     } catch (error) {
       setError(error);
     }
