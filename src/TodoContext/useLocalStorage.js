57f1bd7145aa4a5a5d863b55700d0e341d74b097
jcruzbravo2000 <jcruzbravo2000@gmail.com>
2023-01-08 20:55:12 -0500

    Add file to localStorage

---
+++ src/TodoContext/useLocalStorage.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+// Creación de custom react hook
+function useLocalStorage(itemName, initialValue) {
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(false);
+
+  const [item, setItem] = React.useState(initialValue);
+
+  React.useEffect(() => {
+    try {
+      setTimeout(() => {
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
+
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
+        } else {
+          parsedItem = JSON.parse(localStorageItem);
+        }
+
+        setItem(parsedItem);
+        setLoading(false);
+      }, 1000);
+    } catch (error) {
+      setError(error);
+    }
+  }, [itemName, initialValue]);
+
+  const saveItem = (newItem) => {
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      setItem(newItem);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  return {
+    item,
+    saveItem,
+    loading,
+    error,
+  };
+}
+
+export { useLocalStorage };
